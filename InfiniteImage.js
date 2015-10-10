62cfefd5bfd66dde6e3dbf9541b9e141a622a4d8
OptimisticLock <izgitlab@optimisticlock.com>
2015-10-10 15:27:55 -0700

    palette and canvass

--- InfiniteImage.js
+++ InfiniteImage.js
@@ -2,13 +2,13 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
+  Template.imageEditor.helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
 
-  Template.hello.events({
+  Template.imageEditor.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
