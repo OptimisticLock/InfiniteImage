e5b5f1ed2ae9ecc907338e7e04320b6f611f3253
OptimisticLock <izgitlab@optimisticlock.com>
2015-10-10 15:48:03 -0700

    palette and canvass templates

--- client/InfiniteImage.js
+++ client/InfiniteImage.js
@@ -3,15 +3,9 @@ var count = 0;
 // counter starts at 0
 Session.setDefault('counter', 0);
 
-Template.imageEditor.helpers({
-    counter: function () {
-        return Session.get('counter');
-    }
-});
-
 var isMousedown = false;
 
-Template.imageEditor.events({
+Template.palette.events({
     'click button': function () {
         // increment the counter when button is clicked
         Session.set('counter', Session.get('counter') + 1);
