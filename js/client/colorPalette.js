2fafd39915b9bc25548a836f57b5141c945fd784
OptimisticLock <izgitlab@optimisticlock.com>
2015-10-11 11:20:36 -0700

    color palette works

--- js/client/colorPalette.js
+++ js/client/colorPalette.js
@@ -1,2 +1,13 @@
+Template.colorPalette.helpers({
+    colors: ["black", "white", "gray", "red", "green", "blue", "yellow",
+        "purple", "magenta", "aqua", "aquamarine", "lime"]
+});
 
+Template.colorPalette.events({
+    click: function (event) {
+        var color = event.target.style.backgroundColor;
+        console.log("Clicked", color);
+        Session.set('colorChoice', color);
+    }
+});
 
